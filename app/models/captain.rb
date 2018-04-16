0919123a3a40ca00ad118778b8eed81daaf99bd0
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-16 23:31:20 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,6 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operatiors
-    includes(:boats :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 end
